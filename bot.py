8fbaea2ab1cfb6df9a82d4e81bed4a932e9328a2
Josh Gardner <joshua.patrick.gardner@gmail.com>
2016-05-24 22:29:18 -0700

    update bot
    todos

--- bot.py
+++ bot.py
@@ -1,6 +1,13 @@
 
 #code from https://videlais.com/2015/03/02/how-to-create-a-basic-twitterbot-in-python/
 
+#import HMM model generator, neural net model, 
+#or change TWEET_FILE to file of pre-generated tweets
+
+#TODO: docstrings for functions
+
+#TODO: make this a funcy program??
+
 #load Twitter credentials; you can define below instead
 
 j = json.load(open('../private/twitter_keys.json', 'r'))
@@ -9,7 +16,7 @@ CONSUMER_SECRET = j['CONSUMER_SECRET']
 ACCESS_TOKEN = j['ACCESS_TOKEN']
 ACCESS_SECRET = j['ACCESS_SECRET']
 
-TWEET_FILE = None #define here
+TWEET_FILE = "robotweets.txt"
 
 import tweepy
 
@@ -23,8 +30,16 @@ class TwitterAPI:
         auth.set_access_token(access_token, access_token_secret)
         self.api = tweepy.API(auth)
 
-    def tweet(self, message):
+    def tweet(self):
+        with open(TWEET_FILE, 'r') as robotweets:
+            #TODO: implement prompt user check for tweet approval 
+            #t = robotweets.readline()
+            # print (t)
+
+            # message = t
+
         self.api.update_status(status=message)
+
 
 if __name__ == "__main__":
     twitter = TwitterAPI()
